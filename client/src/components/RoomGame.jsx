43ff381d8fd56d92060b6af928cd13a603031e07
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-09-21 13:20:08 +0100

    updated game action handling

--- client/src/components/RoomGame.jsx
+++ client/src/components/RoomGame.jsx
@@ -8,31 +8,30 @@ function RoomGame() {
     // Create our game
     const game = new Game({ ref:ref });
 
+
     useEffect(() => {
         // On load start the game
         game.start();
-    
+        game.button.button.on('pointerdown', () => handleButton());
+
         // On unload end the game
         return () => {
             game.end();
         };
     }, []);
 
-    const createRoom = () => {
+    const handleButton = () => {
         // game.player.revive(2);
         // game.player.buildShield(["tile027.jpg"]);
         // game.player.drawTavern(1, ["tile027.jpg"]);
         // game.player.drawCastle("tile027.jpg");
-        // game.player.attack(["tile003.jpg"]);
+        game.player.attack(["tile003.jpg"]);
         // game.player.discardShield(["tile016.jpg"]);
         // game.player.discardHand(2, ["tile037.jpg", "tile038.jpg"]);
     }
 
     return (
-        <div>
-            <button onClick={createRoom}>Create A Room</button>
-            <div ref={ref} />
-        </div>
+        <div ref={ref} />
     );
 }
 
