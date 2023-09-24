11c1ab2c0bcf951dde6d30ca8eb178e73936a7ac
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-09-24 14:40:36 +0100

    cleanup

--- client/src/components/RoomGame.jsx
+++ client/src/components/RoomGame.jsx
@@ -31,52 +31,38 @@ function RoomGame({ socket }) {
 
     const handleConfirmButton = () => {
         const selectedCards = {
-            hand: gameRef.current.players["P1"].hand.filter(card => card.selected).map(card => card.name),
-            shield: gameRef.current.players["P1"].shield.filter(card => card.selected).map(card => card.name)
+            hand: gameRef.current.players[socket.id].hand.filter(card => card.selected).map(card => card.name),
+            shield: gameRef.current.players[socket.id].shield.filter(card => card.selected).map(card => card.name)
         }
 
-        gameRef.current.update(
-            {
-                isGameOver: false,
-                players: {
-                    P1: {
-                        isWinner: false,
-                        stance: "waiting",
-                        attackValue: 0,
-                        damageValue: 0,
-                        actions: {
-                            attack: {
-                                units: [...selectedCards.hand, ...selectedCards.shield]
-                            },
-                            revive: {
-                                x: 2
-                            },
-                            buildShield: {
-                                units: "6S"
-                            },
-                            drawTavern: {
-                                x: 0,
-                                units: ["3H", "5D"]
-                            }
-                        } 
-                    },
-                    P2: {
-                        isWinner: false,
-                        stance: "discarding",
-                        attackValue: 0,
-                        damageValue: 0,
-                        actions: {
-                            revive: {
-                                x: 2
-                            },
-                            buildShield: {
-                                unit: "3H"
-                            }
-                        } 
-                    }
+        const gameAction = {
+            isGameOver: false,
+            players: {}
+        };
+
+        gameAction.players[socket.id] = {
+            isWinner: false,
+            stance: "waiting",
+            attackValue: 0,
+            damageValue: 0,
+            actions: {
+                attack: {
+                    units: [...selectedCards.hand, ...selectedCards.shield]
+                },
+                revive: {
+                    x: 2
+                },
+                buildShield: {
+                    units: "6S"
+                },
+                drawTavern: {
+                    x: 0,
+                    units: ["3H", "5D"]
                 }
-            }
-        );
+            } 
+        }
+
+        gameRef.current.update(gameAction);
     }
 
     return (
