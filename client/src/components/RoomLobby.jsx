ee4d2079cd582dee8991372bc1383ecec0714cbb
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-09-25 09:09:23 +0100

    cleanup

--- client/src/components/RoomLobby.jsx
+++ client/src/components/RoomLobby.jsx
@@ -1,11 +1,16 @@
+
+import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
 function RoomLobby({ socket }) {
     const params = useParams();
     const navigate = useNavigate();
+    const [isReady, setIsReady] = useState(false);
 
     const handleReady = () => {
-        socket.emit("handleReady", { roomID: params.roomID, isReady:true });
+        const isCurrentReady = !isReady;
+        setIsReady(isCurrentReady);
+        socket.emit("handleReady", { roomID: params.roomID, isReady:isCurrentReady });
     }
 
     const handleLeave = () => {
@@ -20,7 +25,16 @@ function RoomLobby({ socket }) {
                 </div>
                 <div className='flex justify-between w-full'>
                     <button className="w-2/5 mt-4 px-4 py-2 rounded-lg bg-red-500 hover:bg-red-700 font-black text-lg text-white" onClick={handleLeave} >LEAVE</button>
-                    <button className="w-2/5 mt-4 px-4 py-2 rounded-lg bg-emerald-500 hover:bg-emerald-700 font-black text-lg text-white" onClick={handleReady} >READY</button>
+                    <button
+                        className={`w-2/5 mt-4 px-4 py-2 rounded-lg font-black text-lg text-white
+                            ${
+                                isReady?
+                                'bg-orange-500 hover:bg-orange-700'
+                                :
+                                'bg-emerald-500 hover:bg-emerald-700'
+                            }
+                        `}
+                        onClick={handleReady} >{isReady? "NOT READY": "READY"}</button>
                 </div>
             </div>
         </div>
