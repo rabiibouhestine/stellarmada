c26dc14afc0ea04b581050b2cdc4ce6ad12a5b89
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-09-25 10:38:34 +0100

    cleanup

--- client/src/components/RoomLobby.jsx
+++ client/src/components/RoomLobby.jsx
@@ -8,7 +8,7 @@ function RoomLobby({ socket }) {
     const params = useParams();
     const navigate = useNavigate();
     const [isReady, setIsReady] = useState(false);
-    const [playerDot, setPlayerDot] = useState("");
+    const [playerDotClass, setplayerDotClass] = useState("");
     const [readyBtnClass, setReadyBtnClass] = useState("");
 
     const handleReady = () => {
@@ -22,7 +22,7 @@ function RoomLobby({ socket }) {
     }
 
     useEffect(() => {
-        setPlayerDot(
+        setplayerDotClass(
             isReady?
             "flex justify-center items-center rounded-full w-16 h-16 bg-emerald-500"
             :
@@ -40,15 +40,18 @@ function RoomLobby({ socket }) {
         <div className="flex items-center justify-center h-screen bg-blue-300">
             <div className='grid justify-items-center'>
                 <div className='flex justify-between w-1/3'>
-                    <div className={playerDot}>
+                    <div className={playerDotClass}>
                         <img className='w-12 h-12 mx-auto' src={playerIcon}></img>
                     </div>
-                    <div className={playerDot}>
+                    <div className={playerDotClass}>
                         <img className='w-12 h-12 mx-auto' src={playerIcon}></img>
                     </div>
                 </div>
                 <div className='mb-15 p-6'>
                     <h1 className="text-2xl text-center text-slate-100 font-black drop-shadow-md">WAITING FOR EVERYONE TO BE READY</h1>
+                </div>
+                <div className='mb-15 p-6'>
+                    <h1 className="text-2xl text-center text-slate-100 font-black drop-shadow-md">Invite Link: {window.location.href}</h1>
                 </div>
                 <div className='flex justify-center w-full'>
                     <button className="w-1/5 mx-2 px-4 py-2 rounded-lg bg-red-500 hover:bg-red-700 font-black text-lg text-white" onClick={handleLeave} >
