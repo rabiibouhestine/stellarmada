f2685f12750f32831f9dffb142be0f375366320b
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-09-30 15:20:02 +0100

    cleanup

--- client/src/components/RoomLobby.jsx
+++ client/src/components/RoomLobby.jsx
@@ -2,7 +2,8 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
-import playerIcon from '../images/singleplayer.png';
+import { ClipboardIcon } from '@heroicons/react/24/solid';
+import { UserIcon } from '@heroicons/react/24/solid';
 
 function RoomLobby({ socket }) {
     const params = useParams();
@@ -16,6 +17,10 @@ function RoomLobby({ socket }) {
         setIsReady(isCurrentReady);
         socket.emit("handleReady", { roomID: params.roomID, isReady:isCurrentReady });
     }
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(window.location.href);
+    };
 
     const handleLeave = () => {
         socket.emit("leftRoom");
@@ -39,22 +44,22 @@ function RoomLobby({ socket }) {
 
     return (
         <div className="flex items-center justify-center h-screen bg-blue-300">
-            <div className='grid justify-items-center'>
+            <div className='grid justify-items-center w-2/3'>
                 <div className='flex items-center justify-between w-1/3'>
                     <div className="flex justify-center items-center rounded-full w-16 h-16 bg-sky-600">
-                        <img className='w-12 h-12 mx-auto' src={playerIcon}></img>
+                        <UserIcon className="h-12 w-12 text-white" />
                     </div>
                     <h1 className='text-xl text-center text-slate-100 font-black'> VS </h1>
                     {
                         playersNb === 2 ?
                         <div className="flex justify-center items-center rounded-full w-16 h-16 bg-rose-600">
-                            <img className='w-12 h-12 mx-auto' src={playerIcon}></img>
+                            <UserIcon className="h-12 w-12 text-white" />
                         </div>
                         :
                         <div className="flex justify-center items-center rounded-full w-16 h-16 bg-slate-400"></div>
                     }
                 </div>
-                <div className='mb-15 p-6'>
+                <div className='p-6'>
                     <h1 className="text-2xl text-center text-slate-100 font-black drop-shadow-md">
                     {
                         playersNb === 2 ?
@@ -64,10 +69,16 @@ function RoomLobby({ socket }) {
                     }
                     </h1>
                 </div>
-                <div className='mb-15 p-6'>
-                    <h1 className="text-2xl text-center text-slate-100 font-black drop-shadow-md">Invite Link: {window.location.href}</h1>
+                <div className='my-6 flex justify-center items-center w-full'>
+                    <h1 className="px-4 py-2 rounded-l-lg text-md text-center text-slate-100 font-medium bg-sky-400">
+                        {window.location.href}
+                    </h1>
+                    <button className="px-4 py-2 inline-flex items-center justify-center rounded-r-lg bg-sky-500 hover:bg-sky-700 font-black text-md text-white" onClick={handleCopy} >
+                        <ClipboardIcon className="h-6 w-6 mr-2" />
+                        <span>COPY INVITE LINK</span>
+                    </button>
                 </div>
-                <div className='flex justify-center w-full'>
+                <div className='p-6 flex justify-center w-2/3'>
                     <button className="w-1/5 mx-2 px-4 py-2 rounded-lg bg-red-500 hover:bg-red-700 font-black text-lg text-white" onClick={handleLeave} >
                         LEAVE
                     </button>
@@ -75,7 +86,7 @@ function RoomLobby({ socket }) {
                         {isReady? "UNREADY": "READY"}
                     </button>
                 </div>
-                </div>
+            </div>
         </div>
     );
 }
