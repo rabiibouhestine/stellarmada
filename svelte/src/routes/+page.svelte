70483438c09186172f6a4e6f9090340028039f3a
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-10-13 12:41:33 +0100

    added home page in svelte

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,9 +1,23 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<div class="flex items-center justify-center h-screen bg-slate-600">
+	<div class="grid justify-items-center">
+		<div class="mb-15 p-6">
+			<h1 class="text-6xl text-center text-slate-100 font-black drop-shadow-md">COWARDS CASTLE</h1>
+		</div>
+		<button
+			class="w-1/2 mt-4 px-4 py-2 rounded-lg bg-cyan-600 hover:bg-cyan-500 font-black text-lg text-white"
+		>
+			PLAY AGAINST AI
+		</button>
+		<button
+			class="w-1/2 mt-4 px-4 py-2 rounded-lg bg-emerald-600 hover:bg-emerald-500 font-black text-lg text-white"
+		>
+			CREATE A ROOM
+		</button>
+	</div>
+</div>
 
 <style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
 </style>
