50297c2d56c9073e2a4b659349f63f86390c1919
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-10-16 20:05:56 +0100

    title change

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -21,7 +21,7 @@
 <div class="flex items-center justify-center h-screen bg-slate-600">
 	<div class="grid justify-items-center">
 		<div class="mb-15 p-6">
-			<h1 class="text-6xl text-center text-slate-100 font-black drop-shadow-md">COWARDS CASTLE</h1>
+			<h1 class="text-6xl text-center text-slate-100 font-black drop-shadow-md">FLEET GAMBIT</h1>
 		</div>
 		<!-- <button
 			class="w-1/2 mt-4 px-4 py-2 rounded-lg bg-cyan-600 hover:bg-cyan-500 font-black text-lg text-white"
