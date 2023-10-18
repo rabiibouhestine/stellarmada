20ffcb3c65575ca928ecd5294ec885cd9740f958
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-10-18 11:51:58 +0100

    cleanup

--- client/src/routes/[roomID]/Game.svelte
+++ client/src/routes/[roomID]/Game.svelte
@@ -66,10 +66,12 @@
 	<div id="pixi-container" class="min-w-0 aspect-square" />
 	<div class="flex flex-col min-w-[300px] p-5">
 		<div class="bg-slate-400 w-full h-full rounded-xl" />
-		<div class="flex flex-row space-x-5 h-14 w-full mt-5">
-			<div class="bg-slate-400 w-full rounded-lg" />
-			<div class="bg-slate-400 w-full rounded-lg" />
-			<div class="bg-slate-400 w-full rounded-lg" />
+		<div
+			class="flex flex-row items-center justify-center bg-slate-400 rounded-lg space-x-6 h-20 w-full mt-5"
+		>
+			<div class="bg-slate-500 h-10 w-14 rounded-lg" />
+			<div class="bg-slate-500 h-10 w-14 rounded-lg" />
+			<div class="bg-slate-500 h-10 w-14 rounded-lg" />
 		</div>
 		<div class="bg-slate-400 w-full h-14 rounded-lg mt-5" />
 	</div>
