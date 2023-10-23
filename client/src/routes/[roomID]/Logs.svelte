a3a28779cf8458c38e5893b70ad07abf06f2a949
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-10-23 15:54:29 +0100

    cleanup

--- client/src/routes/[roomID]/Logs.svelte
+++ client/src/routes/[roomID]/Logs.svelte
@@ -18,14 +18,41 @@
 			ncards: 1,
 			location: 'hand',
 			destination: 'frontline'
+		},
+		{
+			id: playerID,
+			cardsNames: ['6C', '5H', '2C', '4C', '9S', '4H'],
+			ncards: 6,
+			location: 'hand',
+			destination: 'frontline'
 		}
 	];
 
-	const getLogColor = (id, playerID) => {
+	const getLogColorClass = (id, playerID) => {
 		if (id === playerID) {
 			return 'bg-apollo-blue-400';
 		} else {
 			return 'bg-apollo-yellow-300';
+		}
+	};
+
+	const getCardColorClass = (cardName) => {
+		const suit = cardName[1];
+		switch (suit) {
+			case 'H':
+				return 'bg-apollo-pink-100 text-apollo-pink-500';
+				break;
+			case 'D':
+				return 'bg-apollo-green-200 text-apollo-green-500';
+				break;
+			case 'S':
+				return 'bg-apollo-yellow-100 text-apollo-yellow-500';
+				break;
+			case 'C':
+				return 'bg-apollo-blue-200 text-apollo-blue-500';
+				break;
+			default:
+				return 'bg-apollo-grey-200 text-apollo-grey-500';
 		}
 	};
 </script>
@@ -37,7 +64,10 @@
 	>
 		{#each moves as move}
 			<div
-				class="px-4 py-2 space-y-2 w-full rounded-lg {getLogColor(move.id, playerID)} bg-opacity-40"
+				class="px-4 py-2 space-y-2 w-full rounded-lg {getLogColorClass(
+					move.id,
+					playerID
+				)} bg-opacity-40"
 			>
 				<div class="text-sm text-white">
 					{#if move.id === 'id1'}
@@ -49,7 +79,8 @@
 				<div class="flex flex-column space-x-1 text-xs font-bold">
 					{#each move.cardsNames as card}
 						<div
-							class="flex px-1 py-2 w-full justify-center content-center rounded-lg bg-apollo-blue-200 text-white"
+							class="flex px-1 py-2 w-full justify-center content-center rounded-lg
+							{getCardColorClass(card)} bg-opacity-80"
 						>
 							{card}
 						</div>
