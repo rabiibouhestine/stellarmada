613b991bdaa2232d67b5ebda30d4be38e7840bb5
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-10-24 18:21:51 +0100

    cleanup

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -26,7 +26,7 @@
 			<h1 class="text-6xl text-center text-slate-100 font-black drop-shadow-md">FLEET GAMBIT</h1>
 		</div>
 		<button
-			class="w-1/2 mt-4 px-4 py-2 rounded-lg bg-white bg-opacity-20 hover:bg-opacity-10 font-black text-lg text-white"
+			class="w-1/2 mt-4 px-4 py-2 rounded-lg bg-white bg-opacity-20 hover:bg-apollo-blue-400 font-black text-lg text-white"
 		>
 			FIND A GAME
 		</button>
@@ -37,7 +37,7 @@
 			CREATE A ROOM
 		</button>
 		<button
-			class="w-1/2 mt-4 px-4 py-2 rounded-lg bg-white bg-opacity-20 hover:bg-opacity-10 font-black text-lg text-white"
+			class="w-1/2 mt-4 px-4 py-2 rounded-lg bg-white bg-opacity-20 hover:bg-apollo-blue-400 font-black text-lg text-white"
 		>
 			PLAY AGAINST AI
 		</button>
