da8cd98300cf1ebe83f60ef8d733ab32215b2535
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-10-25 12:30:51 +0100

    cleanup

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -3,10 +3,6 @@
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 
-	const createRoom = () => {
-		socket.emit('createRoom');
-	};
-
 	onMount(() => {
 		socket.on('createRoomResponse', (data) => {
 			goto(`/${data.room.roomID}`);
@@ -16,6 +12,14 @@
 			socket.off('createRoomResponse');
 		};
 	});
+
+	const createRoom = () => {
+		socket.emit('createRoom');
+	};
+
+	const findGame = () => {
+		goto('/matchmaking');
+	};
 </script>
 
 <div
@@ -27,6 +31,7 @@
 		</div>
 		<button
 			class="w-1/2 mt-4 px-4 py-2 rounded-lg bg-white bg-opacity-20 hover:bg-apollo-blue-400 font-black text-lg text-white"
+			on:click={findGame}
 		>
 			FIND A GAME
 		</button>
