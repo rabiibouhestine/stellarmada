65f61aaa22db686634a29ac88731c6525add926c
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-01 22:22:53 +0100

    cleanup

--- client/src/routes/[roomID]/+page.server.js
+++ client/src/routes/[roomID]/+page.server.js
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export async function load({ cookies, params }) {
     const joinParams = new URLSearchParams({
-        // playerID: cookies.get('playerID'),
+        playerID: cookies.get('playerID'),
         roomID: params.roomID
     });
     const response = await fetch(`http://localhost:3001/join?${joinParams.toString()}`);
