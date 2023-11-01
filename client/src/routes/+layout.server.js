c7066cedf7548aed806fd120623cb187f6c06b38
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-01 20:26:26 +0100

    cleanup

--- client/src/routes/+layout.server.js
+++ client/src/routes/+layout.server.js
@@ -1,6 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
 
 export async function load({ cookies }) {
     if (!cookies.get('playerID')) {
-        cookies.set('playerID', 'playerID', { path: '/' })
+        cookies.set('playerID', uuidv4(), { path: '/' });
+    }
+    return {
+        playerID: cookies.get('playerID')
     }
 }
--- client/src/routes/[roomID]/+page.server.js
+++ client/src/routes/[roomID]/+page.server.js
@@ -1,7 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 
-export async function load({ params }) {
+export async function load({ cookies, params }) {
     const joinParams = new URLSearchParams({
+        playerID: cookies.get('playerID'),
         roomID: params.roomID
     });
     const response = await fetch(`http://localhost:3001/join?${joinParams.toString()}`);
