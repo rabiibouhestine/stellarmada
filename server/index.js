53a7b20e4bdf93514e8b83da49173f052e0dc020
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-02 22:11:22 +0100

    cleanup

--- server/index.js
+++ server/index.js
@@ -194,7 +194,6 @@ app.get('/api/users', (req, res) => {
 });
 
 app.get('/join', (req, res) => {
-    const playerID = req.query.playerID;
     const roomID = req.query.roomID;
 
     // if roomID is not in query params we return error
@@ -214,26 +213,8 @@ app.get('/join', (req, res) => {
         rooms[roomID] = room;
     }
 
-    // if player is already present in a room we return error
-    if (
-        users[playerID] &&
-        users[playerID].room &&
-        rooms[users[playerID].room] &&
-        rooms[users[playerID].room].players[playerID] &&
-        rooms[users[playerID].room].players[playerID].isPresent
-    ) {
-        res.status(500).json({ error: 'player is already present in a room' });
-        return;
-    }
-
     // get room
     const room = rooms[roomID];
-
-    // if room is full we return error
-    if (Object.keys(room.players).length >= 2 && !room.players.hasOwnProperty(playerID)) {
-        res.status(500).json({ error: 'room is full' });
-        return;
-    }
 
     res.json({ gameStarted: room.gameStarted });
 });
