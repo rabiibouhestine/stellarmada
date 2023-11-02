5c93cd6eb8c7d7d4a85dd1d5a564a42d5f7a5699
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-02 14:37:35 +0100

    cleanup

--- client/src/routes/+layout.js
+++ client/src/routes/+layout.js
@@ -1,15 +1,18 @@
 import io from 'socket.io-client';
+import { browser } from "$app/environment"
 
 export function load({ data }) {
-    const socket = io.connect(
-        "http://localhost:3001",
-        {
-            query: {
-                playerID: data.playerID
-              }
-        }    
-    );
-    return {
-        socket: socket
+    if (browser) {
+        const socket = io.connect(
+            "http://localhost:3001",
+            {
+                query: {
+                    playerID: data.playerID
+                }
+            }    
+        );
+        return {
+            socket: socket
+        }
     }
 }
