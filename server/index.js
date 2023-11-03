337937b72dcbc8aa132ade914a247562ef29da6a
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-03 23:55:25 +0100

    cleanup

--- server/index.js
+++ server/index.js
@@ -189,7 +189,8 @@ io.on("connection", (socket) => {
         if (roomID) {
             delete rooms[roomID].players[playerID];
             // if room empty after room update, delete room
-            if (rooms[roomID].players.length === 0) {
+            console.log(rooms[roomID].players.length);
+            if (Object.keys(rooms[roomID].players).length === 0) {
                 delete rooms[roomID];
             } 
         }
