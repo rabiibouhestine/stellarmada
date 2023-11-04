a6475d7cb8852336973a6aec1c1e68862b3f8aa0
Rabii Bouhestine <rabiibouhestine@outlook.com>
2023-11-04 01:42:55 +0100

    cleanup

--- client/src/routes/+error.svelte
+++ client/src/routes/+error.svelte
@@ -1,1 +1,8 @@
-<h1 class="text-white text-9xl flex justify-center">ERROR BIAAATCH</h1>
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="grid justify-items-center text-white">
+	<h1 class="text-9xl">ERROR BIAAATCH</h1>
+	<h1 class="text-xl">{$page.error.message}</h1>
+</div>
